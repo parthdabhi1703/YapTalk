0a94cf6d5e5b9937adf601be39d13c8229a35ba0
Parth Dabhi <dabhiparth309@gmail.com>
2025-02-11 23:09:28 +0530

    Add files via upload

---
+++ nodeServer/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const users = {};
+
+// Serve static files from the project root
+app.use(express.static(path.join(__dirname, '../')));
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+});
+
+server.listen(8000, () => {
+    console.log('Server running on http://127.0.0.1:8000');
+});
